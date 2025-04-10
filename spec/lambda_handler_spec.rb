1d5b4782a901791c8afa84546a28f62c97e6aebd
Delian Petrov <delian7@gmail.com>
2025-04-10 16:57:25 +0000

    linting

--- spec/lambda_handler_spec.rb
+++ spec/lambda_handler_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe 'lambda_handler' do
   let(:context) { {} }
 
   describe 'when invoked with a valid event' do
-    let(:event) { { 'message' => 'Hello, world!', 'httpMethod' => 'GET' } }
     subject(:response) { lambda_handler(event: event, context: context) }
+
+    let(:event) { { 'message' => 'Hello, world!', 'httpMethod' => 'GET' } }
+
 
     it 'returns a status code of 200' do
       expect(response['statusCode']).to eq(200)
@@ -19,8 +21,10 @@ RSpec.describe 'lambda_handler' do
   end
 
   describe 'when invoked with an invalid event' do
-    let(:event) { { 'invalid_key' => 'unexpected data', } }
     subject(:response) { lambda_handler(event: event, context: context) }
+
+    let(:event) { { 'invalid_key' => 'unexpected data' } }
+
 
     it 'returns a status code of 400' do
       expect(response).to include('statusCode')
@@ -34,8 +38,10 @@ RSpec.describe 'lambda_handler' do
   end
 
   describe 'when an exception is raised' do
-    let(:event) { nil }
     subject(:response) { lambda_handler(event: event, context: context) }
+
+    let(:event) { nil }
+
 
     it 'returns a status code of 500' do
       expect(response).to include('statusCode')
